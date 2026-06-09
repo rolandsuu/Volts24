824385e1e9c3134d6e543ae13943a9d139585740
rolandsuu <276161157+rolandsuu@users.noreply.github.com>
2026-06-09 17:11:54 +0800

    chore: prepare trigger production deployment

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
+import { ffmpeg } from "@trigger.dev/build/extensions/core";
 
 export default defineConfig({
   project: "proj_kidytkghtprejkszddib",
   runtime: "node",
   logLevel: "log",
+  build: {
+    extensions: [ffmpeg()],
+  },
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
   // See https://trigger.dev/docs/runs/max-duration
